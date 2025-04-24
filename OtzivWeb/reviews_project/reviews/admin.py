e99c75258734e91b7f4a2cedd50b61b4abf7e541
Mafiozi <139560260+Mafiaozi@users.noreply.github.com>
2025-04-24 22:58:16 +0500

    Delete OtzivWeb directory

--- OtzivWeb/reviews_project/reviews/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'created_at')
-    list_filter = ('created_at', 'user')
-    search_fields = ('name', 'text', 'user__username')
