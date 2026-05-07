73c0ab7517c5ec8c70696d244da9a9716eff20c8
Mafiozi <139560260+Mafiaozi@users.noreply.github.com>
2026-05-07 18:57:21 +0500

    Delete OtzivWeb_VScode directory

--- OtzivWeb_VScode/reviews_project/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import delete_avatar, profile_view
-
-urlpatterns = [
-    path('', profile_view, name='profile'),
-    path('delete-avatar/', delete_avatar, name='delete_avatar'),
-]
